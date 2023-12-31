ebc8c616609df7f759a80e63c276b6eb48640628
ghassen_bougacha <ghassenbougacha@gmail.com>
2023-12-31 16:41:14 +0100

    add backoffice template

---
+++ backoffice/src/layout/components/header/HeaderText.jsx
@@ -0,0 +1,13 @@
+import { Col } from "antd";
+
+export default function HeaderText() {
+  return (
+    <Col
+      xl={16}
+      lg={14}
+      className="hp-header-left-text hp-d-flex-center"
+    >
+
+    </Col>
+  );
+};
