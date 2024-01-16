fa85e3ea71e0e603791819061e2d92c733b73a59
ghass <ghassenbougacha@gmail.com>
2024-01-16 14:14:25 +0100

    react

--- unobackend/unobackend/settings.py
+++ unobackend/unobackend/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 12,
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
