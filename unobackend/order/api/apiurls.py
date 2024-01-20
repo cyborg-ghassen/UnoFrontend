2fe4f2128846465952b0bd7e0a6e4b4ec89a1133
ghass <ghassenbougacha@gmail.com>
2024-01-20 12:59:16 +0100

    order module

--- unobackend/order/api/apiurls.py
+++ unobackend/order/api/apiurls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from.viewsets import OrderItemViewSet
+from.viewsets import OrderItemViewSet, OrderViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'items', OrderItemViewSet, basename='items')
+router.register(r'order', OrderViewSet, basename='order')
 
 urlpatterns = router.urls
--- unobackend/order/models.py
+++ unobackend/order/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from product.models import Product
+
 # Create your models here.
+ORDER_TYPES = (
+    ('physic', 'Physical'),
+    ('company', 'Company'),
+    ('passenger', 'Passenger')
+)
+
+ORDER_STATUS = (
+    ('draft', 'Draft'),
+    ('approved', 'Approved'),
+    ('rejected', 'Rejected')
+)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100, choices=ORDER_TYPES, default='physic')
+    status = models.CharField(max_length=100, choices=ORDER_STATUS, default='draft')
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.type}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.name if self.product else ""} - {self.quantity}'
