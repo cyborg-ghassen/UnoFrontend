1d802e2c8e8766fa62eea0d765c8c08816f30f83
ghass <ghassenbougacha@gmail.com>
2024-02-10 14:44:16 +0100

    images

--- react/src/pages/landing/LandingRightSideNavItem.js
+++ react/src/pages/landing/LandingRightSideNavItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Link, useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {
     Card,
     Dropdown,
