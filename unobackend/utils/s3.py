fe92e6acb7edb979daceecfd7bb78506ffee99a6
Ghassen Bougacha <ghassen.bougacha@gmail.com>
2024-07-01 17:46:07 +0100

    oss

--- unobackend/utils/s3.py
+++ unobackend/utils/s3.py
@@ -1,20 +1,20 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from django_oss_storage.backends import OssStaticStorage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(OssStaticStorage):
     """Used to manage static files for the web server"""
     location = settings.STATIC_LOCATION
     default_acl = 'public-read'
 
 
-class PublicMediaStorage(S3Boto3Storage):
+class PublicMediaStorage(OssStaticStorage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
 
 
-class PrivateMediaStorage(S3Boto3Storage):
+class PrivateMediaStorage(OssStaticStorage):
     location = 'private'
     default_acl = 'private'
     file_overwrite = False
