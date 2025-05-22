8d8394b3bff9b8eccdccd949d3abf96ff03d21ad
Ghassen bougacha <ghassenbougacha@gmail.com>
2025-05-23 00:09:00 +0100

    rm expired page

---
+++ unobackend/atg-tunisie.py
@@ -0,0 +1,65 @@
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+base_url = "https://atg-tunisie.com/fr/"
+
+driver.get(base_url)
+time.sleep(2)
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+category_links = [a["href"] for a in soup.select(".sections-contaier a")]
+
+all_product_data = []
+print(category_links)
+
+for category_url in category_links:
+    driver.get(category_url)
+    time.sleep(2)
+
+    while True:
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        category = soup.select_one("#category-description h2").text.strip() if soup.select_one("#category-description h2") else ""
+        product_links = [a["href"] for a in soup.select(".item .left-product a") if a["href"].startswith(base_url)]
+        print(product_links)
+        for product_url in product_links:
+            driver.get(product_url)
+            time.sleep(2)
+
+            soup = BeautifulSoup(driver.page_source, "lxml")
+            brand = soup.select_one("#product-details a")["href"].split("/")[-1].split('-')[1].capitalize() if soup.select_one("#product-details a") else ""
+            print(brand)
+            product_data = {
+                "title": soup.select_one(".h1").text.strip(),
+                "slogan": soup.select_one(".product-information h2").text.strip() if soup.select_one(".product-information h2") else "",
+                "price": soup.select_one(".product-prices .current-price span")["content"],
+                "description": soup.select_one(".product-information p").text.strip(),
+                "image": soup.select_one("#content .product-cover img")["src"],
+            }
+            print(product_data)
+            all_product_data.append(product_data)
+            driver.back()
+        try:
+            # Wait for pagination to be present (max 3 seconds)
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".pagination"))
+            )
+            next_btn = driver.find_elements(By.CSS_SELECTOR, ".pagination .next")
+            if next_btn and next_btn[0].is_enabled() and next_btn[0].is_displayed():
+                next_btn[0].click()
+                time.sleep(2)
+            else:
+                print("No next button or not enabled/visible.")
+                break
+        except Exception as e:
+            print(f"Pagination not found or next button not clickable")
+            break
+
+driver.quit()
+for product in all_product_data:
+    print(product)
