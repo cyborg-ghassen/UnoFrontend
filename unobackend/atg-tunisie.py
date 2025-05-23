83eae1665e3a836f12e998e187044030d7f08ba0
Ghassen bougacha <ghassenbougacha@gmail.com>
2025-05-24 00:25:41 +0100

    rm expired page

--- unobackend/atg-tunisie.py
+++ unobackend/atg-tunisie.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unobackend.settings")
@@ -24,37 +27,25 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-base_url = "http://www.codifa.net/codifa/en/category/products/"
+base_url = "https://www.sofpince.com.tn/product-category/"
 
 driver.get(base_url)
 time.sleep(2)
 
-soup = BeautifulSoup(driver.page_source, "lxml")
-category_links = [a["href"] for a in soup.select(".list-prod li a")]
-
 all_product_data = []
-print(category_links)
-for category_url in category_links:
-    driver.get(f"{base_url}{category_url}")
+for category in ["chair", "desk"]:
+    driver.get(f"{base_url}{category}")
     time.sleep(2)
 
-    sub_category_links = [a["href"] for a in soup.select("#bas .list-prod li a")]
-    print(sub_category_links)
-    for sub_category_url in sub_category_links:
-        driver.get(f"{base_url}{sub_category_url}")
-        time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, "lxml")
 
-        soup = BeautifulSoup(driver.page_source, "lxml")
-        product_links = [a["href"] for a in soup.select(".list-prod li a")]
-        print(product_links)
-        for product_url in product_links:
-            driver.get(product_url)
+    products = soup.select(".products .product")
+    while True:
+        for product_url in products:
             time.sleep(2)
-
-            soup = BeautifulSoup(driver.page_source, "lxml")
             product_data = {
-                "name": soup.select_one(".col-detail-right .titre-detail").text.strip() if soup.select_one(".col-detail-right .titre-detail") else "",
-                "description": soup.select(".col-detail-right .table-detail .bg-col2 td")[1].text.strip() if soup.select(".col-detail-right .table-detail .bg-col2 td") else "",
+                "name": product_url.select_one("h4 a").text.strip() if product_url.select_one("h4 a") else "",
+                "description": "",
                 "price": 0,
                 "slogan": "",
                 "reviews": 0,
@@ -67,8 +58,8 @@ for category_url in category_links:
                 continue
             else:
                 product = Product.objects.create(**product_data)
-                img_tag = soup.select_one(".col-detail-left img")
-                img_url = urljoin(product_url, img_tag["src"])
+                img_tag = soup.select_one("a img")
+                img_url = urljoin(product_url.select_one("h4 a")['href'], img_tag["src"])
                 img_response = requests.get(img_url)
                 if img_response.status_code == 200:
                     img_temp = NamedTemporaryFile(delete=True)
@@ -81,3 +72,18 @@ for category_url in category_links:
                     product.image.save(f"{product_data['name']}.jpg", ContentFile(img_response.content), save=True)
             print(product_data)
             all_product_data.append(product_data)
+        try:
+            # Wait for pagination to be present (max 3 seconds)
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".woocommerce-pagination ul li"))
+            )
+            next_btn = driver.find_elements(By.CSS_SELECTOR, ".woocommerce-pagination ul li .next")
+            if next_btn and next_btn[0].is_enabled() and next_btn[0].is_displayed():
+                next_btn[0].click()
+                time.sleep(2)
+            else:
+                print("No next button or not enabled/visible.")
+                break
+        except Exception as e:
+            print(f"Pagination not found or next button not clickable")
+            break
