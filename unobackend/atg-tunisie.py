79ea86d2528ac40c2fa0585fed1a7e6852e89bd7
Ghassen bougacha <ghassenbougacha@gmail.com>
2025-05-24 00:01:30 +0100

    rm expired page

--- unobackend/atg-tunisie.py
+++ unobackend/atg-tunisie.py
@@ -24,13 +24,13 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-base_url = "https://sodet.com.tn/"
+base_url = "http://www.codifa.net/codifa/en/category/products/"
 
 driver.get(base_url)
 time.sleep(2)
 
 soup = BeautifulSoup(driver.page_source, "lxml")
-category_links = [a["href"] for a in soup.select("#menu-item-322 .sub-menu a")]
+category_links = [a["href"] for a in soup.select(".list-prod li a")]
 
 all_product_data = []
 print(category_links)
@@ -38,56 +38,46 @@ for category_url in category_links:
     driver.get(category_url)
     time.sleep(2)
 
-    soup = BeautifulSoup(driver.page_source, "lxml")
-    product_links = [a["href"] for a in soup.select(".box-image .image-zoom_in a")]
-    print(product_links)
-    for product_url in product_links:
-        driver.get(product_url)
+    sub_category_links = [a["href"] for a in soup.select(".list-prod li a")]
+    print(sub_category_links)
+    for sub_category_url in sub_category_links:
+        driver.get(sub_category_url)
         time.sleep(2)
 
         soup = BeautifulSoup(driver.page_source, "lxml")
-        categories = soup.select(".product-info .product_meta span a")
+        product_links = [a["href"] for a in soup.select(".list-prod li a")]
+        print(product_links)
+        for product_url in product_links:
+            driver.get(product_url)
+            time.sleep(2)
 
-        brand = soup.select_one(".product-info h1").text.strip() if soup.select_one(".product-info h1") else ""
-        print(brand)
-        if Brand.objects.filter(name=brand).exists():
-            brand_instance = Brand.objects.filter(name=brand).first()
-        else:
-            brand_instance = Brand.objects.create(name=brand)
-        product_data = {
-            "name": soup.select_one(".product-info h1").text.strip() if soup.select_one(".product-info h1") else "",
-            "description": soup.select_one(".product-info .product-short-description p").text.strip() if soup.select_one(".product-info .product-short-description p") else "",
-            "price": 0,
-            "slogan": "",
-            "reviews": 0,
-            "stock": 0,
-            "promotion": 0,
-            "brand": brand_instance,
-        }
-        if Product.objects.filter(**product_data).exists():
-            product = Product.objects.filter(**product_data).first()
-            print(f"Product already exists: {product_data['name']}")
-            continue
-        else:
-            product = Product.objects.create(**product_data)
-            for category in categories:
-                category_name = category.text.strip()
-                if not Category.objects.filter(name=category_name).exists():
-                    category_instance = Category.objects.create(name=category_name)
-                else:
-                    category_instance = Category.objects.get(name=category_name)
-                product.category.add(category_instance)
-            img_tag = soup.select_one(".woocommerce-product-gallery__wrapper .woocommerce-product-gallery__image a img")
-            img_url = urljoin(product_url, img_tag["src"])
-            img_response = requests.get(img_url)
-            if img_response.status_code == 200:
-                img_temp = NamedTemporaryFile(delete=True)
-                img_temp.write(img_response.content)
-                img_temp.flush()
+            soup = BeautifulSoup(driver.page_source, "lxml")
+            product_data = {
+                "name": soup.select_one(".col-detail-right .titre-detail").text.strip() if soup.select_one(".col-detail-right .titre-detail") else "",
+                "description": soup.select(".col-detail-right .table-detail .bg-col2 td")[1].text.strip() if soup.select(".col-detail-right .table-detail .bg-col2 td") else "",
+                "price": 0,
+                "slogan": "",
+                "reviews": 0,
+                "stock": 0,
+                "promotion": 0,
+            }
+            if Product.objects.filter(**product_data).exists():
+                product = Product.objects.filter(**product_data).first()
+                print(f"Product already exists: {product_data['name']}")
+                continue
+            else:
+                product = Product.objects.create(**product_data)
+                img_tag = soup.select_one(".col-detail-left img")
+                img_url = urljoin(product_url, img_tag["src"])
+                img_response = requests.get(img_url)
+                if img_response.status_code == 200:
+                    img_temp = NamedTemporaryFile(delete=True)
+                    img_temp.write(img_response.content)
+                    img_temp.flush()
 
-                # Save to a Django model
+                    # Save to a Django model
 
-                # Assuming your model has an ImageField called 'image'
-                product.image.save(f"{soup.select_one('.product-info h1').text.strip()}.jpg", ContentFile(img_response.content), save=True)
-        print(product_data)
-        all_product_data.append(product_data)
+                    # Assuming your model has an ImageField called 'image'
+                    product.image.save(f"{product_data['name']}.jpg", ContentFile(img_response.content), save=True)
+            print(product_data)
+            all_product_data.append(product_data)
