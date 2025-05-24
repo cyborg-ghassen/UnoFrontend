9ccd1b3c0cc1fad7f0890ce6821ac20783f6378e
Ghassen bougacha <ghassenbougacha@gmail.com>
2025-05-24 10:25:34 +0100

    rm expired page

--- unobackend/atg-tunisie.py
+++ unobackend/atg-tunisie.py
@@ -27,47 +27,44 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-base_url = "https://www.sofpince.com.tn/product-category/"
+base_url = "https://bsp.com.tn/fr/category"
 
 driver.get(base_url)
 time.sleep(2)
 
 all_product_data = []
-for category in ["chair", "desk"]:
-    driver.get(f"{base_url}{category}/page/3/")
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+products = soup.select("#MixItUp1E2E08 .gallery-item")
+for product_url in products:
     time.sleep(2)
+    product_data = {
+        "name": product_url.select_one(".overlay-inner .name_product a").text.strip() if product_url.select_one(".overlay-inner .name_product a") else "",
+        "description": "",
+        "price": 0,
+        "slogan": "",
+        "reviews": 0,
+        "stock": 0,
+        "promotion": 0,
+    }
+    if Product.objects.filter(**product_data).exists():
+        product = Product.objects.filter(**product_data).first()
+        print(f"Product already exists: {product_data['name']}")
+        continue
+    else:
+        product = Product.objects.create(**product_data)
+        img_tag = product_url.select_one(".image-box img")
+        img_url = urljoin(product_url.select_one(".overlay-inner .name_product a")['href'], img_tag["src"])
+        img_response = requests.get(img_url)
+        if img_response.status_code == 200:
+            img_temp = NamedTemporaryFile(delete=True)
+            img_temp.write(img_response.content)
+            img_temp.flush()
 
-    soup = BeautifulSoup(driver.page_source, "lxml")
+            # Save to a Django model
 
-    products = soup.select(".products .product")
-    for product_url in products:
-        time.sleep(2)
-        product_data = {
-            "name": product_url.select_one("h4 a").text.strip() if product_url.select_one("h4 a") else "",
-            "description": "",
-            "price": 0,
-            "slogan": "",
-            "reviews": 0,
-            "stock": 0,
-            "promotion": 0,
-        }
-        if Product.objects.filter(**product_data).exists():
-            product = Product.objects.filter(**product_data).first()
-            print(f"Product already exists: {product_data['name']}")
-            continue
-        else:
-            product = Product.objects.create(**product_data)
-            img_tag = product_url.select_one("a img")
-            img_url = urljoin(product_url.select_one("h4 a")['href'], img_tag["src"])
-            img_response = requests.get(img_url)
-            if img_response.status_code == 200:
-                img_temp = NamedTemporaryFile(delete=True)
-                img_temp.write(img_response.content)
-                img_temp.flush()
-
-                # Save to a Django model
-
-                # Assuming your model has an ImageField called 'image'
-                product.image.save(f"{product_data['name']}.jpg", ContentFile(img_response.content), save=True)
-        print(product_data)
-        all_product_data.append(product_data)
+            # Assuming your model has an ImageField called 'image'
+            product.image.save(f"{product_data['name']}.jpg", ContentFile(img_response.content), save=True)
+    print(product_data)
+    all_product_data.append(product_data)
